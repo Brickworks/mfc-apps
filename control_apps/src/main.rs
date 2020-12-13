dae860f0dfaadb62538cc5f70943ece9a691bfae
Philip Linden <philiplinden@planet.com>
2020-12-13 12:09:18 -0800

    CTRL: clean up state transitions

--- control_apps/src/main.rs
+++ control_apps/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, thread, time};
 
 enum ControlMode {
     Initializing,
@@ -26,62 +26,35 @@ struct Controller {
     state: ControlMode,
 }
 
+struct Valve {
+    pwm: f32,
+    locked: bool,
+}
+
 impl Controller {
     fn init() -> Self {
         Controller { 
-            state: ControlMode::Initializing
+            state: ControlMode::Initializing,
         }
     }
-    fn cycle(&mut self) {
-        let next = match self.state {
-            ControlMode::Initializing => {
-                ControlMode::Safe
-            }
-            ControlMode::Safe => {
-                ControlMode::Idle
-            }
-            ControlMode::Idle => {
-                ControlMode::Raising
-            }
-            ControlMode::Raising => {
-                ControlMode::Lowering
-            }
-            ControlMode::Lowering => {
-                ControlMode::Idle
-            }
-            ControlMode::Dumping => {
-                ControlMode::Safe
-            }
-        };
-        self.state = next;
-        println!("{:}", self.state);
+    fn transition(&mut self, target_state:ControlMode) {
+        println!("Control Mode: {:} --> {:}", self.state, target_state);
+        self.state = target_state;
     }
     fn safe(&mut self) {
-        self.state = ControlMode::Safe;
-        println!("{:}", self.state);
+        self.transition(ControlMode::Safe);
     }
     fn abort(&mut self) {
-        self.state = ControlMode::Dumping;
-        println!("{:}", self.state);
+        self.transition(ControlMode::Dumping);
+        println!("(waiting 5 seconds to dump ballast...)");
+        thread::sleep(time::Duration::from_secs(5)); // wait 5 seconds
+        self.transition(ControlMode::Safe);
     }
 }
 
 fn main() {
     let mut controller = Controller::init();
     // test state transitions here
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.cycle();
-    controller.abort();
-    controller.cycle();
-    controller.cycle();
     controller.safe();
-    
+    controller.abort();
 }
