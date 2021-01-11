582216d65b42cbe100bb67ebed15f137d504abfb
Philip Linden <lindenphilipj@gmail.com>
2021-01-11 15:36:04 -0800

    CTRL: clean up bad merge for state machine

--- control_apps/src/main.rs
+++ control_apps/src/main.rs
@@ -1,10 +1,6 @@
-use std::{fmt, thread, time};
+use std::fmt;
 
-<<<<<<< HEAD
-#[derive(Eq, PartialEq)]
-=======
 #[derive(Copy, Clone)]
->>>>>>> CTRL: state machine first try
 enum ControlMode {
     Init,
     Safe,
@@ -77,48 +73,6 @@ struct Controller {
 
 impl Controller {
     fn init() -> Self {
-<<<<<<< HEAD
-        Controller { 
-            state: ControlMode::Initializing,
-        }
-    }
-    fn transition(&mut self, target_state:ControlMode) {
-        if self.state == target_state {
-            println!("Control Mode: (still) {:}", self.state);
-            self.state = target_state; // technically a no-op
-        } else {
-            println!("Control Mode: {:} --> {:}", self.state, target_state);
-            self.state = target_state;
-        }
-    }
-    fn safe(&mut self) {
-        self.transition(ControlMode::Safe);
-    }
-    fn abort(&mut self) {
-        let mut wait_ticker = 0;
-        loop {
-            if wait_ticker >= 5 {
-                break;
-            } else {
-                self.transition(ControlMode::Dumping);
-                println!("(waiting {:} seconds for ballast to finish dumping...)", 5-wait_ticker);
-                thread::sleep(time::Duration::from_secs(1)); // wait 5 seconds
-                wait_ticker = wait_ticker + 1;
-            }
-        }
-        self.transition(ControlMode::Safe);
-    }
-    fn idle(&mut self) {
-        self.transition(ControlMode::Idle);
-    }
-    fn stabilize(&mut self) {
-        // run control algorithm every N ticks to refresh control effort
-        // change mode to Raising if control effort wants to raise altitude
-        // change mode to Lowering if control effort wants to lower altitude
-        // change mode to Idle if altitude is within controller "Dead zone"
-        // TODO: allow loop to be interrupted by other reinforced commands
-        println!("The code for the Stabilize command hasn't been written yet!");
-=======
         Controller {
             state: ControlMode::Init,
             pwm_vent: 0,
@@ -178,39 +132,12 @@ impl Controller {
         self.pwm_vent = 0;
         self.pwm_ballast = 0;
         println!("{:} \t| pwm vent: {} \t| pwm ballast: {}", self.state, self.pwm_vent, self.pwm_ballast);
->>>>>>> CTRL: state machine first try
     }
 }
 
 fn main() {
+    let mut controller = Controller::init();
     // test state transitions here
-<<<<<<< HEAD
-    let mut controller = Controller::init(); // initialize the controller
-    controller.safe(); // start in safe mode
-    controller.idle(); // transition to idle
-    controller.stabilize(); // ups and downs
-    controller.idle(); // reassert idle
-    controller.abort(); // abort and end in safe
-
-    // test valves here
-    let mut bleed_valve = Valve::init(0, "BleedValve".to_string());
-    let mut ballast_valve = Valve::init(99, "BallastValve".to_string());
-    bleed_valve.lock();
-    ballast_valve.lock();
-    ballast_valve.unlock();
-    if bleed_valve.is_locked() == true {
-        println!("{:} is locked!", bleed_valve.name);
-    } else {
-        println!("{:} is unlocked!", bleed_valve.name);
-    }
-    bleed_valve.unlock();
-    println!("{:} PWM is {:}", ballast_valve.name, ballast_valve.pwm);
-    ballast_valve.set_pwm(0.5);
-    println!("{:} PWM is {:}", ballast_valve.name, ballast_valve.pwm);
-    ballast_valve.lock();
-    ballast_valve.set_pwm(0.5);
-    println!("{:} PWM is {:}", ballast_valve.name, ballast_valve.pwm);
-=======
     controller.cycle();
     controller.cycle();
     controller.cycle();
@@ -225,6 +152,5 @@ fn main() {
     controller.cycle();
     controller.cycle();
     controller.safe();
->>>>>>> CTRL: state machine first try
     
 }
