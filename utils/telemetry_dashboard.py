183edfac14cdbb99f14943fbaad73ae8bcd09f95
Philip Linden <lindenphilipj@gmail.com>
2022-02-23 00:40:50 -0800

    DASH: add more data to the dashboard and organize it a little better

--- utils/telemetry_dashboard.py
+++ utils/telemetry_dashboard.py
@@ -17,29 +17,49 @@ PLOT_TEMPLATE = 'plotly_white'
 # -----------------------------------------------------------------------------
 app = Dash(__name__, )
 app.layout = html.Div([
-    dcc.Dropdown(id='tail_lines',
-                 options=[
-                     {
-                         'label': '100 samples',
-                         'value': 100
-                     },
-                     {
-                         'label': '1000 samples',
-                         'value': 1000
-                     },
-                     {
-                         'label': '10000 samples',
-                         'value': 10000
-                     },
-                 ],
-                 value=1000),
-    dcc.Interval(
-        id='interval-component',
-        interval=1000,  # in milliseconds
-        n_intervals=0),
-    # store the last N data points
-    dcc.Store(id='last-n-lines'),
-    dcc.Graph(id='altitude'),
+    html.Div([
+        # dashboard setup
+        dcc.Dropdown(id='tail_lines',
+                     options=[
+                         {
+                             'label': '100 samples',
+                             'value': 100
+                         },
+                         {
+                             'label': '1000 samples',
+                             'value': 1000
+                         },
+                         {
+                             'label': '10000 samples',
+                             'value': 10000
+                         },
+                     ],
+                     value=1000),
+        dcc.Interval(
+            id='interval-component',
+            interval=1000,  # in milliseconds
+            n_intervals=0),
+        # store the last N data points
+        dcc.Store(id='last-n-lines'),
+    ]),
+    html.Div([
+        html.Div([
+            # motion
+            dcc.Graph(id='altitude'),
+            dcc.Graph(id='ascent_rate'),
+            dcc.Graph(id='acceleration'),
+        ]),
+        html.Div([
+            # mass properties
+            dcc.Graph(id='lift_gas_mass'),
+            dcc.Graph(id='ballast_mass'),
+        ]),
+        html.Div([
+            # altitude control
+            dcc.Graph(id='vent_pwm'),
+            dcc.Graph(id='dump_pwm'),
+        ])
+    ], style={'display': 'flex', 'flex-direction': 'row'}),
 ])
 
 
@@ -63,6 +83,21 @@ def get_last_n_lines_from_csv(intervals, n_lines, fname=DEFAULT_DATA_CSV):
                        ]).to_json()
 
 
+def simple_scatter(df, name):
+    fig = go.Figure()
+    fig.update_layout(
+        title=name,
+        xaxis_title='Time (s)',
+        template=PLOT_TEMPLATE,
+    )
+    fig.add_trace(
+        go.Scattergl(x=df['time_s'],
+                    y=df[name],
+                    mode='markers+lines',
+                    name=name))
+    return fig
+
+
 # Multiple components can update everytime interval gets fired.
 @app.callback(
     Output('altitude', 'figure'),
@@ -71,18 +106,67 @@ def get_last_n_lines_from_csv(intervals, n_lines, fname=DEFAULT_DATA_CSV):
 )
 def altitude(interval, data):
     df = pd.read_json(data)
-    fig = go.Figure()
-    fig.update_layout(
-        title='Altitude (m)',
-        xaxis_title='Time (s)',
-        template=PLOT_TEMPLATE,
-    )
-    fig.add_trace(
-        go.Scattergl(x=df['time_s'],
-                     y=df['altitude_m'],
-                     mode='markers+lines',
-                     name='altitude_m'))
-    return fig
+    return simple_scatter(df, 'altitude_m')
+
+
+@app.callback(
+    Output('ascent_rate', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def ascent_rate(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'ascent_rate_m_s')
+
+
+@app.callback(
+    Output('acceleration', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def acceleration(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'acceleration_m_s2')
+
+
+@app.callback(
+    Output('lift_gas_mass', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def lift_gas_mass(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'lift_gas_mass_kg')
+
+
+@app.callback(
+    Output('ballast_mass', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def ballast_mass(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'ballast_mass_kg')
+
+
+@app.callback(
+    Output('vent_pwm', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def vent_pwm(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'vent_pwm')
+
+
+@app.callback(
+    Output('dump_pwm', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    Input('last-n-lines', 'data'),
+)
+def dump_pwm(interval, data):
+    df = pd.read_json(data)
+    return simple_scatter(df, 'dump_pwm')
 
 
 if __name__ == '__main__':
