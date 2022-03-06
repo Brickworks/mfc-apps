e01eabe7deeabb38b86ba0ae028fa1703c7e404f
Philip Linden <lindenphilipj@gmail.com>
2022-03-05 21:20:11 -0800

    CLI: compatibility with updated API for sysinfo modern version

--- cli/src/status.rs
+++ cli/src/status.rs
@@ -1,5 +1,5 @@
 use log::{trace, debug, info, warn, error};
-use sysinfo::SystemExt;
+use sysinfo::{ComponentExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
 enum LogLevel {
     Debug,
@@ -38,45 +38,49 @@ fn print_report(messages: Vec<ReportMessage>) {
 
 fn sys_report() -> Vec<ReportMessage> {
     let mut msgs: Vec<ReportMessage> = vec![];
-    let mut system = sysinfo::System::new();
 
-    // First we update all information of our system struct.
-    system.refresh_all();
-    
+    // Please note that we use "new_all" to ensure that all list of
+    // components, network interfaces, disks and users are already
+    // filled!
+    let mut sys = System::new_all();
+
+    // First we update all information of our `System` struct.
+    sys.refresh_all();
+
     // // Now let's print every process' id and name:
     // for (pid, proc_) in system.get_process_list() {
     //     println!("{}:{} => status: {:?}", pid, proc_.name, proc_.status);
     // }
     
     // Then let's print the temperature of the different components:
-    for component in system.get_components_list() {
+    for component in sys.components() {
         let msg_content = format!("Temperaure of {:?}", &component);
-        let critical_temp = component.critical.unwrap_or(1.5*component.max);
-        let msg_level = match component.temperature {
+        let critical_temp = component.critical().unwrap_or(1.5*component.max());
+        let msg_level = match component.temperature() {
             t if t >= critical_temp => LogLevel::Error,
-            t if t >= 0.8 * component.max => LogLevel::Warn,
+            t if t >= 0.8 * component.max() => LogLevel::Warn,
             _ => LogLevel::Info,
         };
         msgs.push(ReportMessage{
             level: msg_level,
             content: msg_content,
         });
-        msgs.push(match component.critical {
+        msgs.push(match component.critical() {
             Some(ref critical_temp) => {
                 ReportMessage{
                     level: LogLevel::Debug,
-                    content: format!("Detected critical temp for {:}: {:?}", component.label, critical_temp),
+                    content: format!("Detected critical temp for {:}: {:?}", component.label(), critical_temp),
                 }},
             None => {
                 ReportMessage{
                     level: LogLevel::Debug,
-                    content: format!("Estimated critical temp for {:}: {:?}", component.label, 1.5*component.max),
+                    content: format!("Estimated critical temp for {:}: {:?}", component.label(), 1.5*component.max()),
                 }},
             });
     }
     
     // And then all disks' information:
-    for disk in system.get_disks() {
+    for disk in sys.disks() {
         msgs.push(ReportMessage{
             level: LogLevel::Info,
             content: format!("{:?}", disk),
@@ -86,19 +90,19 @@ fn sys_report() -> Vec<ReportMessage> {
     // And finally the RAM and SWAP information:
     msgs.push(ReportMessage{
         level: LogLevel::Info,
-        content: format!("total memory: {} kB", system.get_total_memory()),
+        content: format!("total memory: {} kB", sys.total_memory()),
     });
     msgs.push(ReportMessage{
         level: LogLevel::Info,
-        content: format!("used memory : {} kB", system.get_used_memory()),
+        content: format!("used memory : {} kB", sys.used_memory()),
     });
     msgs.push(ReportMessage{
         level: LogLevel::Info,
-        content: format!("total swap  : {} kB", system.get_total_swap()),
+        content: format!("total swap  : {} kB", sys.total_swap()),
     });
     msgs.push(ReportMessage{
         level: LogLevel::Info,
-        content: format!("used swap   : {} kB", system.get_used_swap()),
+        content: format!("used swap   : {} kB", sys.used_swap()),
     });
     return msgs
 }
